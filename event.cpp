9c1c169b3235b80d6f3ea0ef11e488752613348a
Giap Tran <txgvnn@gmail.com>
2016-06-08 12:34:46 +0700

    add event.*

---
+++ event.cpp
@@ -0,0 +1,77 @@
+#include "event.h"
+
+bool Event::control[5] = {false, false, false, false, false}; //{UP,RIGHT, LEFT, DOWN, ATTACK}
+
+SDL_Event Event::event;
+
+int Event::checkEvent(void *data){
+
+	while (1){
+		while( SDL_PollEvent( &Event::event ) != 0 )
+		{
+			//User requests quit
+			if( Event::event.type == SDL_QUIT )
+			{
+				// quit = true;
+				std::cout<<"quit"<<std::endl;
+			}
+			//User presses a key
+			if( Event::event.type == SDL_KEYDOWN )
+			{
+				//Select surfaces based on key press
+				switch( Event::event.key.keysym.sym )
+				{
+					case SDLK_UP:
+					std::cout<<"UP"<<std::endl;
+					Event::control[0] = true;
+					break;
+
+					case SDLK_DOWN:
+					std::cout<<"DOWN"<<std::endl;
+					Event::control[2] = true;
+					break;
+
+					case SDLK_LEFT:
+					std::cout<<"LEFT"<<std::endl;
+					Event::control[1] = true;
+					break;
+
+					case SDLK_RIGHT:
+					std::cout<<"RIGHT"<<std::endl;
+					Event::control[3] = true;
+					break;
+
+				}
+			}
+			if( Event::event.type == SDL_KEYUP )
+			{
+				//Select surfaces based on key press
+				switch( Event::event.key.keysym.sym )
+				{
+					case SDLK_UP:
+					std::cout<<"U_UP"<<std::endl;
+					Event::control[0] = false;
+					break;
+
+					case SDLK_DOWN:
+					std::cout<<"U_DOWN"<<std::endl;
+					Event::control[2] = false;
+					break;
+
+					case SDLK_LEFT:
+					std::cout<<"U_LEFT"<<std::endl;
+					Event::control[1] = false;
+					break;
+
+					case SDLK_RIGHT:
+					std::cout<<"U_RIGHT"<<std::endl;
+					Event::control[3] = false;
+					break;
+
+				}
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ event.h
@@ -0,0 +1,13 @@
+#ifndef _EVENT_H_
+#define _EVENT_H_
+#include "utils.h"
+
+class Event
+{
+
+public:
+	static SDL_Event event;
+	static bool control[5];
+	static int checkEvent(void *data);
+};
+#endif
